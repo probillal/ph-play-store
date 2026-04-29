b8b84ad0fdcce62effb002927bcc6204c5c6d878
probillal <smbillalhossain273@gmail.com>
2026-04-29 20:32:22 +0600

    show more btn added

--- src/Components/HomePage/TrendingApps.jsx
+++ src/Components/HomePage/TrendingApps.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import AppsCard from "../UI/AppsCard";
 
 const TrendingApps = ({ data }) => {
@@ -7,10 +8,17 @@ const TrendingApps = ({ data }) => {
       <p className="text-gray-400 text-center">
         Explore All Trending Apps on the Market developed by us
       </p>
-      <div className="container mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-4 my-16">
-        {data.map((apps, index) => (
+      <div className="container mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
+        {data.slice(0, 9).map((apps, index) => (
           <AppsCard key={index} apps={apps}></AppsCard>
         ))}
+      </div>
+      <div className="text-center mt-6">
+        <Link to="/apps">
+          <button className="bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white p-4 rounded-md">
+            Show More
+          </button>
+        </Link>
       </div>
     </div>
   );
